13d14930157a5844743971d14955301311dcad34
DmtrVed <veddima2020@gmail.com>
2026-09-12 21:19:15 +0700

    init commit

--- first_lesson/task_16.py
+++ first_lesson/task_16.py
@@ -1,1 +1,12 @@
 def month_calendar(start_weekday, days):
+    table = ""
+    week_counter = start_weekday
+    table += " " * 2 * week_counter
+    table += " " * (week_counter - 1)
+    for i in range(days):
+        if week_counter == 7 and i != 0:
+            table += "\n"
+            week_counter = 0
+        table += f" {i+1:2}" if week_counter != 0 else f"{i+1:2}"
+        week_counter += 1
+    return table
